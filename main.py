763a33ecdd067a3ec49b95d8c95b9304dc6685d4
Conqu3red <Conqu3red@users.noreply.github.com>
2021-03-16 19:54:39 +0000

    made things look nicer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import typing
+import sys
+from reprint import output
 
 with open("elements.json", "rb") as f:
     data = json.load(f)
@@ -34,32 +36,55 @@ def solve_sentence(sentence: str):
     return result
 
 if __name__ == "__main__":
-    with open('words.txt') as word_file:
-        valid_words = set(word_file.read().split())
+    with output(output_type='dict') as output_lines:
+        with open('words.txt') as word_file:
+            valid_words = word_file.read().split()
 
-    best_score = {"symbols":"", "score":0}
-    longest_word = {"symbols":"", "score":0}
-    best_ratio = {"symbols":"", "score":0, "ratio":0}
-    i = 0
-    l = len(valid_words)
-    for word in valid_words:
-        for result in solve_word(word):
-            if result["score"] > best_score["score"]:
-                print(f"New best score: {result['symbols']} : {result['score']}")
-                best_score = result
+        best_score = {"symbols":"", "score":0}
+        longest_word = {"symbols":"", "score":0}
+        best_ratio = {"symbols":"", "score":0, "ratio":0}
+        amazing_words = []
+        best_word_at_length = {}
 
-            if len(result["symbols"]) > len(longest_word["symbols"]):
-                print(f"New longest: {result['symbols']} : {result['score']}")
-                longest_word = result
+        i = 0
+        l = len(valid_words)
 
-            if result["score"]/len(result["symbols"]) > best_ratio["ratio"]:
-                result["ratio"] = result["score"]/len(result["symbols"])
-                print(f"Best ratio: {result['symbols']} : {result['score']} : {result['ratio']}")
-                best_ratio = result
-        i += 1
-        if i % 10_000 == 0:
-            print(f"{i}/{l}")
-    print(f"Best score: {best_score}")
-    print(f"Longest word: {longest_word}")
-    print(f"Best ratio: {best_ratio}")
+        def rewrite_progress():  
+            output_lines["Progress"] = f"{int((i/l)*100)}% ({i}/{l})"
+            output_lines["Best Score"] = f"{best_score['symbols']} score: {best_score['score']}"
+            output_lines["Longest Word"] = f"{longest_word['symbols']} score: {longest_word['score']} length: {len(longest_word['symbols'])}"
+            #output_lines["Good words"] = ", ".join([f"{w['symbols']} ({w['score']})" for w in amazing_words])
+        for word in valid_words:
+            best = {"score":0}
+            for result in solve_word(word):
+                if result["score"] > best_score["score"]:
+                    rewrite_progress()
+                    best_score = result
+
+                if len(result["symbols"]) > len(longest_word["symbols"]):
+                    rewrite_progress()
+                    longest_word = result
+
+                if result["score"]/len(result["symbols"]) > best_ratio["ratio"]:
+                    result["ratio"] = result["score"]/len(result["symbols"])
+                    rewrite_progress()
+                    best_ratio = result
+                if result["score"] > best["score"]:
+                    best = result
+                if not best_word_at_length.get(len(result["symbols"])) or result["score"] > best_word_at_length.get(len(result["symbols"]))["score"]:
+                    best_word_at_length[len(result["symbols"])] = result
+            if best and best["score"] > 1500:
+                amazing_words.append(best)
+            i += 1
+            if i % 10_000 == 0:
+                rewrite_progress()
+        rewrite_progress()
     print("Done.")
+
+    print("Best word at every length:")
+    for k, w in {k:v for k,v in sorted(best_word_at_length.items())}.items():
+        print(f"  {k} - {w['symbols'].lower()} - {w['symbols']} ({w['score']})")
+    print("Words that score above 1500:")
+    for w in amazing_words:
+        print(f"  {w['symbols'].lower()} - {w['symbols']} ({w['score']})")
+        
